ec9510dc13f94358c2a015b6a7843d28959e75c9
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2012-01-14 12:01:35 +0000

    Added function to remove label in pages_content.html

--- static/js/pages.js
+++ static/js/pages.js
@@ -29,7 +29,7 @@
 		    this.number_of_label = len - 1;
 		    
 		},
-    	init_add_page : function(){
+    	init_add_label : function(){
     	    
 		    t.get('.submit_one').on('click', function(e){
 		        e.preventDefault();
@@ -38,13 +38,25 @@
 		            'url' : '/admin/pages/add_label/'+pages.number_of_label+'/',
 		            'success' : function(data){
 		                    t.get('.submit_one').before(data);
+		                    t.get('.submit_two').before(data);
 		                    pages.add_on_change(pages.number_of_label);
 		            }
 		        });		            
 		    });
 		    
     	},
+    	init_remove_label : function(){
+    	    
+    	    t.get('.page_remove_label').on('click', function(e){
+    	        e.preventDefault();
+                var button_click = e.currentTarget.value;
+                var num_label = t.get(this).attr('class').split('_')[3];
+                t.get('.section_num_'+num_label).destroy() 
+	        });
+    	    
+    	},
     	add_on_change : function(i) {
+    	    
     	    t.get('.label_'+i+'_0').on('change', function(e){
                 var chosen = e.currentTarget.value;
                 var chosen_num = e.currentTarget.name.split('_')[2];
@@ -53,13 +65,17 @@
                 t.get('.label_'+chosen_num+'_3').css({ 'display' : 'none' })
                 t.get('.label_'+chosen_num+'_'+chosen).css({ 'display' : 'inline' })
 	        });
+	        
     	}
 	}
 	
 	t.get(document).ready(function(){
+	    
 		pages.init();
 		pages.init_change_label();
-		pages.init_add_page();
+		pages.init_add_label();
+		pages.init_remove_label();
+		
 	})
 		
 })();
