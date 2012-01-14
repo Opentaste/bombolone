cfd3bd24b1d1db01f565057d259d70ed068821fd
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2012-01-14 19:57:47 +0000

    Added page_new_page in admin.py
    Initial version of the tool to create the pages, and splitted pages_content_page logic request in other function.

--- admin.py
+++ admin.py
@@ -109,8 +109,71 @@ def pages_page():
     """
 
     """
-    list_pages = g.db.pages.find()
-    return render_template('admin/pages.html', pages=list_pages)
+    pages = g.db.pages.find()
+    return render_template('admin/pages.html', **locals())
+    
+def page_request_form(page):
+    """
+    """
+    len_of_label = len([x for x in request.form if x.startswith('label_it')]) / 2
+    
+    if g.my['rank'] is 10:
+        page['name'] = request.form['name']
+        page['file'] = request.form['name_file']
+        url_it = request.form['url_it']
+        url_en = request.form['url_en']
+        page['url'] = { 'it' : url_it, 'en' : url_en }
+        page['input_label'] = [ int(request.form['input_label_'+str(i)]) for i in range(len_of_label)]
+        
+    title_it = request.form['title_it']
+    title_en = request.form['title_en']
+
+    description_it = request.form['description_it']
+    description_en = request.form['description_en']
+
+    page['title'] = { 'it' : title_it, 'en' : title_en }
+    page['description'] = { 'it' : description_it, 'en' : description_en }
+    page['content'] = { 'it' : [], 'en' : [] }
+
+    for i in range(len_of_label):
+        label = 'label_it_'+str(i)
+        if 'label_it_name_0' in request.form:
+            label = request.form['label_it_name_'+str(i)]
+        alias = request.form['alias_it_name_'+str(i)]
+        if page['input_label'][i] is 3:
+            name_file = upload_file('it_'+str(i), 'page')
+            page['content']['it'].append( { 'label' : label, 'alias' : alias, 'value' : name_file })
+        else:
+            page['content']['it'].append( { 'label' : label, 'alias' : alias, 'value' : request.form['label_it_'+str(i)] })
+
+    for i in range(len_of_label):
+        label = 'label_en_'+str(i)
+        if 'label_en_name_0' in request.form:
+            label = request.form['label_en_name_'+str(i)]
+        alias = request.form['alias_en_name_'+str(i)]
+        if page['input_label'][i] is 3:
+            name_file = upload_file('en_'+str(i), 'page')
+            page['content']['en'].append( { 'label' : label, 'alias' : alias, 'value' : name_file } )
+        else:
+            page['content']['en'].append( { 'label' : label, 'alias' : alias, 'value' : request.form['label_en_'+str(i)] })
+            
+    return page
+
+@check_authentication    
+def page_new_page():
+    """
+    
+    """
+    if g.my['rank'] is not 10:
+        abort(401)
+    if request.method == 'POST':
+        page = {}
+        
+        page = page_request_form(page)
+        
+        g.db.pages.insert( page )
+        
+    return render_template('admin/pages_new.html')
   
 def pages_content_page(_id):
     """
@@ -122,49 +185,8 @@ def pages_content_page(_id):
         
         page = g.db.pages.find_one({ '_id' : ObjectId(_id) })
         
-        len_of_label = len([x for x in request.form if x.startswith('label_it')]) / 2
+        page = page_request_form(page)
         
-        if g.my['rank'] is 10:
-            page['name'] = request.form['name']
-            page['file'] = request.form['name_file']
-            url_it = request.form['url_it']
-            url_en = request.form['url_en']
-            page['url'] = { 'it' : url_it, 'en' : url_en }
-            page['input_label'] = [ int(request.form['input_label_'+str(i)]) for i in range(len_of_label)]
-            
-        title_it = request.form['title_it']
-        title_en = request.form['title_en']
-
-        description_it = request.form['description_it']
-        description_en = request.form['description_en']
-
-        page['title'] = { 'it' : title_it, 'en' : title_en }
-        page['description'] = { 'it' : description_it, 'en' : description_en }
-        page['content'] = { 'it' : [], 'en' : [] }
-
-        for i in range(len_of_label):
-            label = 'label_it_'+str(i)
-            print label
-            if 'label_it_name_0' in request.form:
-                label = request.form['label_it_name_'+str(i)]
-            alias = request.form['alias_it_name_'+str(i)]
-            if page['input_label'][i] is 3:
-                name_file = upload_file('it_'+str(i), 'page')
-                page['content']['it'].append( { 'label' : label, 'alias' : alias, 'value' : name_file })
-            else:
-                page['content']['it'].append( { 'label' : label, 'alias' : alias, 'value' : request.form['label_it_'+str(i)] })
-
-        for i in range(len_of_label):
-            label = 'label_en_'+str(i)
-            if 'label_en_name_0' in request.form:
-                label = request.form['label_en_name_'+str(i)]
-            alias = request.form['alias_en_name_'+str(i)]
-            if page['input_label'][i] is 3:
-                name_file = upload_file('en_'+str(i), 'page')
-                page['content']['en'].append( { 'label' : label, 'alias' : alias, 'value' : name_file } )
-            else:
-                page['content']['en'].append( { 'label' : label, 'alias' : alias, 'value' : request.form['label_en_'+str(i)] })
-                  
         g.db.pages.update( { '_id' : ObjectId(_id) }, page)
         
     page_content = g.db.pages.find_one({ '_id' : ObjectId(_id) })
