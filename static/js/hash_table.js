25ff9ba48e907efba97aa3a7d20ea9641b66be9b
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2012-01-26 00:51:59 +0000

    Rewrote the background-image of the buttons

---
+++ static/js/hash_table.js
@@ -0,0 +1,18 @@
+(function(){
+	var hash_Table = {
+		init : function(){
+		    
+			
+			
+		},
+		number_document : 0
+		
+	}
+	
+	t.get(d).ready(function(){
+	    
+		hash_Table.init();
+		
+	})
+		
+})();
